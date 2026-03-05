1fa2065ff4f8cbe38169d81b199b88b161349749
mengsifei <victoriameng2013@gmail.com>
2026-03-05 21:07:35 +0000

    restructure build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 readonly VERSION_ROS2="ROS2"
-readonly VERSION_HUMBLE="humble"
 
 pushd `pwd` > /dev/null
 cd `dirname $0`
 echo "Working Path: "`pwd`
 
-ROS_HUMBLE=""
-
-# Set working ROS version (ROS2 only)
-if [ "$1" = "ROS2" ] || [ -z "$1" ]; then
-    :
-elif [ "$1" = "humble" ]; then
-    ROS_HUMBLE=${VERSION_HUMBLE}
-else
-    echo "Invalid argument. Use: ROS2|humble"
-    exit
+# Set working ROS version (ROS2 Iron+ only)
+if [ -n "$1" ] && [ "$1" != "ROS2" ]; then
+    echo "Invalid argument. Use: ROS2 (or empty)"
+    exit 1
 fi
-echo "ROS version is: ${VERSION_ROS2}"
+echo "ROS version is: ${VERSION_ROS2} (Iron+)"
 
 # clear `build/` folder.
 # TODO: Do not clear these folders, if the last build is based on the same ROS version.
@@ -32,20 +25,16 @@ fi
 
 # exit
 
-# substitute the files/folders: package.xml, launch/
+# substitute package.xml
 if [ -f package.xml ]; then
     rm package.xml
 fi
 cp -f package_ROS2.xml package.xml
-cp -rf launch_ROS2/ launch/
 
 # build
 pushd `pwd` > /dev/null
 cd ../../
-colcon build --cmake-args -DHUMBLE_ROS=${ROS_HUMBLE}
+colcon build
 popd > /dev/null
-
-# remove the substituted folders/files
-rm -rf launch/
 
 popd > /dev/null
